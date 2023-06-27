9ea0940e3cd64824f07bd596e0b08e64123c696f
Prajapati-Shivam <prajapatishivam11203@gmail.com>
2023-06-27 18:41:07 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 var cors = require('cors')
 
-const port = process.env.REACT_APP_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
